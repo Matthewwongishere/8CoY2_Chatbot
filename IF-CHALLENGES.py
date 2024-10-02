91b258413b941869b58835e93c52f29a34096c42
Matthewwongishere <mawong@kellettschool.com>
2024-10-02 04:03:27 +0000

    Wed, Oct 2, 2024, 4:03 AM +00:00

---
+++ IF-CHALLENGES.py
@@ -0,0 +1,9 @@
+print("Welcome to DA MONKEY THEME PARK")
+print("ARE YOU TALL ENOUGH FOR THE MONSTA RIDE?")
+height = input("PLEASE INPUT YOUR HEIGHT (IN CM WITHOUT THE CM SYMBOL), SORRY IF YOU ARE TOO UNWORTHY...")
+if height >= "120" or height:
+    print("YOU ARE CAPABLE OF RIDING THIS RIDE!!!  HAVE FUN... WEEEEEEEEEEEE....")
+if height < "120":
+    print("SORRY LITTLE BOI, YOU ARE UNWORTHY...")
+else:
+    print("UMM.......")
