0ec0dd79b73aaff0ff3d7fcf1e3987c70cf0b13b
Matthewwongishere <mawong@kellettschool.com>
2024-10-09 04:06:34 +0000

    Wed, Oct 9, 2024, 4:06 AM +00:00

---
+++ WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
@@ -0,0 +1,18 @@
+#Adventure Game
+
+welcome = "Welcome adventurer!"
+print(welcome)
+
+forest = "you find yourself in a deep dark forest"
+print(forest)
+
+#move north, east, south or west
+move = input("which direction do you want to go?")
+
+if move == "north":
+    mountains = "you are in a snowy mountain, you see a cave"
+    print(mountains)
+
+elif move == "east":
+    plains = "you find yourself in a grassy plain, there's a tree nearby"
+    print(plains)
