48859520c8df1a68bc1b1a40b853ba9bda7e5314
Matthewwongishere <mawong@kellettschool.com>
2024-10-09 04:12:06 +0000

    Wed, Oct 9, 2024, 4:12 AM +00:00

--- WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
+++ WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
@@ -1,18 +1,20 @@
 #Adventure Game
 
-welcome = "Welcome adventurer!"
-print(welcome)
-
+#LOCATIONS / MESSAGES
+mountains = "you are in a snowy mountain, you see a cave"
+plains = "you find yourself in a grassy plain, there's a tree nearby"
 forest = "you find yourself in a deep dark forest"
+welcome = "Welcome adventurer!"
+cave = "you enter a pitch black cave, you can't see anything"
+
+#LOGIC
+print(welcome)
 print(forest)
-
-#move north, east, south or west
 move = input("which direction do you want to go?")
-
 if move == "north":
-    mountains = "you are in a snowy mountain, you see a cave"
     print(mountains)
-
+    goCave = input("Would you like to enter the cave?")
+    if goCave == "yes":
+                   print(cave)
 elif move == "east":
-    plains = "you find yourself in a grassy plain, there's a tree nearby"
     print(plains)
