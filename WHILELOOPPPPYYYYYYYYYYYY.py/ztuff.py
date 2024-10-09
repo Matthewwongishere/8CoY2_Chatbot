00a2370f16e32af4b0dc963ea2318497cfdf9dda
Matthewwongishere <mawong@kellettschool.com>
2024-10-09 04:19:49 +0000

    Wed, Oct 9, 2024, 4:19 AM +00:00

--- WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
+++ WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
@@ -6,7 +6,8 @@ plains = "you find yourself in a grassy plain, there's a tree nearby"
 forest = "you find yourself in a deep dark forest"
 welcome = "Welcome adventurer!"
 cave = "you enter a pitch black cave, you can't see anything"
-torchcave = "you use the flashlight and see there's a golden statue in the centre of the cave."
+torchcave = "you use the flashlight and see there's a D3AD BODY! in the centre of the cave."
+inventory = []
 
 #LOGIC
 print(welcome)
@@ -16,8 +17,17 @@ if move == "north":
     print(mountains)
     goCave = input("Would you like to enter the cave?")
     if goCave == "yes":
-                   if hasTorch:
+                   if "torch" in inventory:
                            print(torchcave)
                    print(cave)
 elif move == "east":
     print(plains)
+    if not "torch" in inventory:
+            print("there's a torch on the floor")
+            pickup = input("do you want to pick up the torch? ")
+            if pickup == "yes":
+                    inventory.append("torch")
+lookin = input("Do you want to look at your inventory?")
+if lookin == "yes":
+        print(inventory)
+
