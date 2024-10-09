4f1966b35fbc81bcabd82f20c9f3dfb3b2b0121c
Matthewwongishere <mawong@kellettschool.com>
2024-10-09 04:41:07 +0000

    Wed, Oct 9, 2024, 4:41 AM +00:00

--- WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
+++ WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
@@ -9,7 +9,7 @@ cave = "you enter a pitch black cave, you can't see anything"
 torchcave = "you use the flashlight and see there's a D3AD BODY! in the centre of the cave."
 inventory = []
 punch = ("you have performed a punch, HAI-YAA!")
-fist = ("just a thingy")
+fist = ("fist")
 songy = ("Look at the stars Look how they shine for you And everything you do Yeah, they were all yellow I came along I wrote a song for you And all the things you do And it was called Yellow So then I took my turn Oh, what a thing to have done And it was all yellow Your skin, oh yeah, your skin and bones Turn into something beautiful And you know, you know I love you so You know I love you so I swam across I jumped across for you Oh, what a thing to do 'Cause you were all yellow I drew a line I drew a line for you Oh, what a thing to do And it was all yellow And your skin, oh yeah, your skin and bones Turn into something beautiful And you know, for you, I'd bleed myself dry For you, I'd bleed myself dry It's true Look how they shine for you Look how they shine for you Look how they shine for Look how they shine for you Look how they shine for you Look how they shine Look at the stars Look how they shine for youAnd all the things that you do")
 
 #LOGIC
@@ -46,5 +46,6 @@ if song == "no" or song == "NO":
 else:
         print("I am going to give it to you anyways!")
         print(songy)
+        print("                                                                                                          THE END")
 
 
