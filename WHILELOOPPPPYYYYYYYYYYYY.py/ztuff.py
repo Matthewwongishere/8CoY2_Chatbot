a7907ae408a72039eec30d5d9b507ec8c0f9c884
Matthewwongishere <mawong@kellettschool.com>
2024-10-09 04:14:09 +0000

    Wed, Oct 9, 2024, 4:14 AM +00:00

--- WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
+++ WHILELOOPPPPYYYYYYYYYYYY.py/ztuff.py
@@ -6,6 +6,7 @@ plains = "you find yourself in a grassy plain, there's a tree nearby"
 forest = "you find yourself in a deep dark forest"
 welcome = "Welcome adventurer!"
 cave = "you enter a pitch black cave, you can't see anything"
+torchcave = "you use the flashlight and see there's a golden statue in the centre of the cave."
 
 #LOGIC
 print(welcome)
@@ -15,6 +16,8 @@ if move == "north":
     print(mountains)
     goCave = input("Would you like to enter the cave?")
     if goCave == "yes":
+                   if hasTorch:
+                           print(torchcave)
                    print(cave)
 elif move == "east":
     print(plains)
