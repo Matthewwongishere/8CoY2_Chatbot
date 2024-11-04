df348a4019fd689a2f4d53df3c072eb1d6baa9c4
Matthewwongishere <mawong@kellettschool.com>
2024-11-04 06:57:26 +0000

    Mon, Nov 4, 2024, 6:57 AM +00:00

--- Imagewriter.py
+++ Imagewriter.py
@@ -2,11 +2,12 @@
 
 img = open("testimage.ppm","w")
 
-img.write("P6") #magic number
+img.write("P3") #magic number
 img.write("640 480") # width and height
 img.write("255") # Max colour depth
 
 for pixel in range(640):
+    img.write("255 0 0 ")
 
 
 img.close()
