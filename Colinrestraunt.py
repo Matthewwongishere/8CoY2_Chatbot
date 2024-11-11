abd5e6af2ca0f8c3bccc5a30fd3dc3c887c83f09
Matthewwongishere <mawong@kellettschool.com>
2024-11-11 06:42:04 +0000

    Mon, Nov 11, 2024, 6:42 AM +00:00

---
+++ Colinrestraunt.py
@@ -0,0 +1,6 @@
+print("welcome to the colin reatarant")
+fatty = input("Is COLIN cool?")
+if fatty == "Yes" or fatty == "Yes":
+    print("YAS,QUEEEN")
+else:
+    print("U ARE UNSKIBIDI")
---
+++ respect.py
@@ -0,0 +1,16 @@
+#respect.pY
+
+# variable
+name = "Colin2.0"
+age = 12
+
+phrase1 = "Happy Birthday to you"
+# for method
+for _ in range(2):
+    print(phrase1)
+print(f"Happy Birthday dear {name}")
+print(phrase1)
+for 
+
+
+
